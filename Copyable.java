09ccc6a03b1c5030bed2a233a9f6289b66fc3a6c
nakednous <nakednous@gmail.com>
2014-02-01 19:42:22 -0500

    Squashed 'src/remixlab/util/' content from commit 30583e7
    git-subtree-dir: src/remixlab/util
    git-subtree-split: 30583e7c63e6d68b3e8bd781ec86c44ce168c18c

---
+++ Copyable.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * TerseHandling (version 1.0.0)
+ * Copyright (c) 2014 National University of Colombia, https://github.com/remixlab
+ * @author Jean Pierre Charalambos, http://otrolado.info/
+ *     
+ * All rights reserved. Library that eases the creation of interactive
+ * scenes, released under the terms of the GNU Public License v3.0
+ * which is available at http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+package remixlab.util;
+
+public interface Copyable {
+	public Object get();
+}
